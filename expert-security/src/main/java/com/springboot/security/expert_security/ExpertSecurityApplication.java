7fee083e343ae34192fc0f12e6e65250c84ae549
alexander1914 <alexander.oliveira99@gmail.com>
2024-05-29 15:55:04 +0100

    Implementando o SecurityConfig, SenhaMasterAuthenticationProvider, e o CustomFilter e aprendendo o processo de autenticacao - part 2

---
+++ expert-security/src/main/java/com/springboot/security/expert_security/ExpertSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.security.expert_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertSecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertSecurityApplication.class, args);
+	}
+
+}
---
+++ expert-security/src/test/java/com/springboot/security/expert_security/ExpertSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.security.expert_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
